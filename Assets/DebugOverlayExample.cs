fb4d617f4f9b5bc75207b55b4bbad838622374c2
Evan Rosson <github@erosson.org>
2014-04-29 01:43:24 -0700

    split main into separate example files

---
+++ Assets/DebugOverlayExample.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using ERosson;
+
+public class DebugOverlayExample : MonoBehaviour {
+	// DebugOverlay has no dependency on LoadLevelArgs; we're just using this as an example of some real-ish data we might want to debug
+	public LoadLevelArgsExample example;
+
+	void Start () {
+		// DebugOverlay examples. It's this easy.
+		DebugOverlay.Watch("datetime.now", () => System.DateTime.Now);
+		DebugOverlay.Watch("loadCount", () => example.loadCount);
+    }
+}
---
+++ Assets/LoadLevelArgsExample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using ERosson;
+
+// Args passed via loadlevel. You can use any class or struct.
+class ExampleArgs {
+	public int loadCount = 0;
+}
+
+public class LoadLevelArgsExample : MonoBehaviour {
+	// initial value is not used, the default LoadLevelArgs overwrites it
+	public int loadCount = -999;
+
+	void Start () {
+		// LoadLevelArgs.Pop requires a default value, so levels can always be run from the editor safely for testing.
+		// By default, that's the zero-arg constructor of the type you pass here. Pop(defaultValue) also works.
+		var param = LoadLevelArgs.Pop<ExampleArgs>();
+		loadCount = param.loadCount;
+	}
+
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		// A button, to force a reload of the same level.
+		if (GUILayout.Button("load count: " + loadCount + ". Reload level")) {
+			// 
+			var args = new ExampleArgs();
+			args.loadCount = loadCount + 1;
+			LoadLevelArgs.LoadLevel(Application.loadedLevelName, args);
+		}
+		GUILayout.EndArea();
+	}
+}
