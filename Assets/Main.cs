bddb8832193a185083b50c70541171c890138413
Evan Rosson <github@erosson.org>
2014-04-29 00:13:44 -0700

    example main scene, tests loadlevelargs with a button

---
+++ Assets/Main.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using ERosson;
+
+class MainParam {
+	public int loadCount = 0;
+}
+
+public class Main : MonoBehaviour {
+	// initial value is not used, the default LoadLevelArgs overwrites it
+	public int loadCount = -999;
+
+	void Start () {
+		// LoadLevelArgs.Pop requires a default value, so levels can always be run from the editor safely for testing
+		var param = LoadLevelArgs.Pop<MainParam>();
+		loadCount = param.loadCount;
+	}
+
+	void OnGUI() {
+		GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+		if (GUILayout.Button("load count: " + loadCount + ". Reload level")) {
+			var param = new MainParam();
+			param.loadCount = loadCount + 1;
+			LoadLevelArgs.LoadLevel(Application.loadedLevelName, param);
+		}
+		GUILayout.EndArea();
+	}
+}
